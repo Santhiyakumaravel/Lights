8e45a5b0010b2b5f4c1e5d34bcb42f59750baea3
Santhiyakumaravel <santhiyaaiml@gmail.com>
2025-03-22 14:04:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var body=document.querySelector("body");
+function dark()
+{
+    body.className="";
+body.classList.add("dark")
+}
+
+function light()
+{
+    body.className="";
+    body.classList.add("light")
+}
